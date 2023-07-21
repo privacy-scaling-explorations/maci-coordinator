cca904b2571a07ab9bd620f3bd52d1c7828ef220
Daehyun Paik <paik@a30a.dev>
2023-07-21 02:34:27 +0200

    Add wallet integration

--- client/maci-coordinator-react/src/App.tsx
+++ client/maci-coordinator-react/src/App.tsx
@@ -19,30 +19,23 @@ import {
 } from '@rainbow-me/rainbowkit';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
-	mainnet,
-	polygon,
-	optimism,
-	arbitrum,
-	zora,
+	foundry
 } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient } = configureChains(
-	[mainnet, polygon, optimism, arbitrum, zora],
+	[foundry],
 	[
-		// alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
 		publicProvider()
 	]
 );
-const { connectors } = getDefaultWallets({
-	appName: 'My RainbowKit App',
-	projectId: 'YOUR_PROJECT_ID',
-	chains
-});
+// const { connectors } = getDefaultWallets({
+// 	appName: 'My RainbowKit App',
+// 	projectId: 'YOUR_PROJECT_ID',
+// 	chains
+// });
 const wagmiConfig = createConfig({
-	autoConnect: true,
-	connectors,
+	autoConnect: false,
 	publicClient
 })
 
--- client/maci-coordinator-react/src/pages/SubmitPage/index.tsx
+++ client/maci-coordinator-react/src/pages/SubmitPage/index.tsx
@@ -1,16 +1,64 @@
 
 
-import { Button, HStack, VStack } from '@chakra-ui/react';
+import { Button, HStack, Text, VStack } from '@chakra-ui/react';
 // import React, { useEffect, useState } from 'react';
 import { useApi } from '../../context/ApiContext';
 
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
+import { useContractRead } from 'wagmi';
 
+
+import { verifierContractABI } from './verifierContractABI';
+import { useState } from 'react';
+
+// import { VerifyingKey } from 'maci-domainobjs'
+
+// const myProof = ["16542799721131651594785520319939783050030996408880254975213942397115447948314", "11276094094407219125404880012996672687090029887517677365047394352730647638307", "4624707048990030241056274829986006064451294258864122130035851765417778857786", "11374867336108506564620695974673178140916932693816778303485868343126183996044", "13333897898351739374526247260562782187584657865084138075076474604866795747683", "21796399198891278713705094768108457173296477518126790625214500606487714231431", "12623440252842916725366345337807414063060721420474747555220467655269698182186", "3227599174012473946974486383698113016637468848831011083098752465254352579452"];
+
+// const myPublicInput = [
+// 	"18723775248480812191782918525766489928877804881253575454956449323181176067888"
+// ]
+
+// const processVk: VerifyingKey = VerifyingKey.fromJSON(vkObj)
+
+// WIll make a hooks using WAGMI for verifier contract
+// https://wagmi.sh/react/typescript
+// https://wagmi.sh/react/hooks/useContractWrite
+// https://wagmi.sh/react/hooks/useContractRead
 // SubmitPage will call verifier contract of Ethereum
 const SubmitPage: React.FC<React.PropsWithChildren<{}>> = () => {
+	console.log("------SubmitPage mounted-----")
 	// it hooks into the context(state variables)
 	const { proofs, proverStateProcessMessages } = useApi();
+	const verifierContractAddress = '0x'
+
+
+	// const [verifyProofData, setVerifyProofData] = useState<any>(null);
+	// const [verifyProofError, setVerifyProofError] = useState<any>(null);
+	// const [verifyProofLoading, setVerifyProofLoading] = useState(false);
+
+	const handleClick = () => {
+		// setVerifyProofLoading(true);
+		console.log("Verify proof onchain");
+	};
+
+	// useContractRead({
+	// 	address: verifierContractAddress,
+	// 	abi: verifierContractABI,
+	// 	functionName: 'verify',
+	// 	args: [myProof, 'FIXME_VK', myPublicInput],
+	// 	onError: (error) => {
+	// 		setVerifyProofError(error);
+	// 		setVerifyProofLoading(false);
+
+	// 		console.log("verifyProofError", verifyProofError);
+	// 		console.log("verifyProofLoading", verifyProofLoading);
+	// 	},
+	// });
+
+
+
 
 	const handleSomething = () => {
 		console.log("handleSomething");
@@ -18,16 +66,17 @@ const SubmitPage: React.FC<React.PropsWithChildren<{}>> = () => {
 
 	return (
 		<VStack>
-			  <ConnectButton />;
-			<HStack alignSelf="stretch" justifyContent="space-between" minH={"42px"} alignItems={"center"} spacing={10}>
-				<Button onClick={handleSomething}>Do</Button>
-			</HStack>
+			<Text>prover status: {proverStateProcessMessages}</Text>
+
+			<ConnectButton />;
+
+			<Button onClick={handleClick}>VerifyProof</Button>
 			<>
-					{/* display current prover status */}
-					<div>prover status: {proverStateProcessMessages}</div>
-					{/* render all vars */}
-					<div>proofs: {JSON.stringify(proofs, null, 2)}</div>
-				</>
+				{/* display current prover status */}
+				<div>prover status: {proverStateProcessMessages}</div>
+				{/* render all vars */}
+				<div>proofs: {JSON.stringify(proofs, null, 2)}</div>
+			</>
 		</VStack>
 
 	);
